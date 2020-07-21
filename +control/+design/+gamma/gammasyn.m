e2171b593ac592941da4fc7afa52a297d3097c80
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-07-21 17:39:39 +0200

    return all gains except for only proportional gain is designed
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- +control/+design/+gamma/gammasyn.m
+++ +control/+design/+gamma/gammasyn.m
@@ -530,15 +530,10 @@ function [Ropt, Jopt, information] = gammasyn(systems, areafun, weights, R_fixed
 		end
 	end
 	[R_opt, K_opt, F_opt] = x2R(x_opt, dimensions_strict);
-	if dimensions_strict.measurements_xdot > 0
+	if dimensions_strict.measurements_xdot > 0 || dimensions_strict.references > 0
 		Ropt = {
 			R_opt;
 			K_opt;
-			F_opt
-		};
-	elseif dimensions_strict.references > 0
-		Ropt = {
-			R_opt;
 			F_opt
 		};
 	else
