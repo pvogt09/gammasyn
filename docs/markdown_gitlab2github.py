6de18a5badb72023475b303308a6d0066da5c539
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-07-29 13:01:19 +0200

    adds PEP8 conformity to markdown_gitlab2github.py
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- docs/markdown_gitlab2github.py
+++ docs/markdown_gitlab2github.py
@@ -14,21 +14,21 @@ readmes_mdtex = glob.glob(r"README.md.tex", recursive=False)
 
 readme = r""
 if len(readmes_texmd) > 1:
-    raise Error('More than one README.tex.md exist')
+    raise Exception('More than one README.tex.md exist')
 if len(readmes_mdtex) > 1:
-    raise Error('More than one README.md.tex exist')
+    raise Exception('More than one README.md.tex exist')
 if not readmes_texmd:
     if not readmes_mdtex:
-        raise Error('Neither README.tex.md nor README.md.tex exist')
+        raise Exception('Neither README.tex.md nor README.md.tex exist')
     else:
         readme = readmes_mdtex[0]
 else:
     if not readmes_mdtex:
         readme = readmes_texmd[0]
     else:
-        raise Error('Both README.tex.md and README.md.tex exist')
+        raise Exception('Both README.tex.md and README.md.tex exist')
 if not readme:
-    raise Error('No readme file found')
+    raise Exception('No readme file found')
 readme = directory + os.sep + readme
 readme_out = directory + os.sep + 'README.md'
 
@@ -37,9 +37,17 @@ with open(readme, 'r') as file:
     content = file.read()
 
 # replace content
-content = re.sub(r'(```math)(.*?)(```)', r'$$\2$$', content, flags=re.MULTILINE | re.DOTALL)
+content = re.sub(
+    r'(```math)(.*?)(```)',
+    r'$$\2$$',
+    content,
+    flags=re.MULTILINE | re.DOTALL)
 # at minimum one blank line before formula
-content = re.sub(r'([^\r\n]+[\r\n])(\$\$)(.*?)(\$\$)', r'\1\n$$\3$$', content, flags=re.MULTILINE | re.DOTALL)
+content = re.sub(
+    r'([^\r\n]+[\r\n])(\$\$)(.*?)(\$\$)',
+    r'\1\n$$\3$$',
+    content,
+    flags=re.MULTILINE | re.DOTALL)
 content = re.sub(r'(\$`)(.*?)(`\$)', r'$\2$', content)
 
 # write file
