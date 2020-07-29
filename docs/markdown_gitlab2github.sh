2a37bc8b02c02aed820513f2747d5b6f92085740
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-07-29 20:26:00 +0200

    adds conditions for pull requests and push events to update links to target
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- docs/markdown_gitlab2github.sh
+++ docs/markdown_gitlab2github.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 set -e
+if [ -z "$1" ]; then
+	targetbranch="$1"
+else
+	targetbranch=
+fi
 if [ "${GITHUB_REPOSITORY}" = "" ]; then
 	projectname=gammasyn
 	username=pvogt09
@@ -20,3 +25,6 @@ python ./docs/markdown_gitlab2github.py "$(realpath ./)" || exit 1
 python -m readme2tex --svgdir docs/svgs --project "$projectname" --username "$username" --output "README.md" "README.tex.md" || exit 2
 sed -i "s#https://rawgit.com/$username/$projectname/None#https://raw.githubusercontent.com/$username/$projectname/$branchname#" "README.md"
 sed -i "s#https://rawgit.com/$username/$projectname/$branchname#https://raw.githubusercontent.com/$username/$projectname/$branchname#" "README.md"
+if [ -z $targetbranch ]; then
+	sed -i "s#https://raw.githubusercontent.com/$username/$projectname/$branchname#https://raw.githubusercontent.com/$username/$projectname/$targetbran#" "README.md"
+fi
