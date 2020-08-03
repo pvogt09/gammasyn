ba2f47d5dc50313c37b8befcbcf336d14955fe21
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-08-03 16:22:03 +0200

    adds Gitlab markdown hint to CONTRIBUTING.md
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- docs/markdown_gitlab2github.sh
+++ docs/markdown_gitlab2github.sh
@@ -28,7 +28,6 @@ else
 	echo "Creating documentation for PR from ${branchname} to ${targetbranch}"
 fi
 python ./docs/markdown_gitlab2github.py "$(realpath ./)" || exit 1
-git diff "README.tex.md"
 python -m readme2tex --svgdir "docs/svgs" --project "$projectname" --username "$username" --output "README.md" "README.tex.md" || exit 2
 git checkout -- "README.tex.md" || exit 3
 sed -i "s#https://rawgit.com/$username/$projectname/None#https://raw.githubusercontent.com/$username/$projectname/$branchname#" "README.md"
