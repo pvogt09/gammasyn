6ee5a41a2863f93a44d0d597795e34471d149709
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-08-03 16:40:00 +0200

    adds GITHUB_BASE_REF and GITHUB_HEAD_REF to sed
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- docs/markdown_gitlab2github.sh
+++ docs/markdown_gitlab2github.sh
@@ -32,6 +32,8 @@ python -m readme2tex --svgdir "docs/svgs" --project "$projectname" --username "$
 git checkout -- "README.tex.md" || exit 3
 sed -i "s#https://rawgit.com/$username/$projectname/None#https://raw.githubusercontent.com/$username/$projectname/$branchname#" "README.md"
 sed -i "s#https://rawgit.com/$username/$projectname/$GITHUB_REF#https://raw.githubusercontent.com/$username/$projectname/$branchname#" "README.md"
+sed -i "s#https://rawgit.com/$username/$projectname/$GITHUB_HEAD_REF#https://raw.githubusercontent.com/$username/$projectname/$branchname#" "README.md"
+sed -i "s#https://rawgit.com/$username/$projectname/$GITHUB_BASE_REF#https://raw.githubusercontent.com/$username/$projectname/$branchname#" "README.md"
 sed -i "s#https://rawgit.com/$username/$projectname/$branchname#https://raw.githubusercontent.com/$username/$projectname/$branchname#" "README.md"
 if [ -n "$targetbranch" ]; then
 	sed -i "s#https://raw.githubusercontent.com/$username/$projectname/$branchname#https://raw.githubusercontent.com/$username/$projectname/$targetbranch#" "README.md"
