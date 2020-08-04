4e3ba4640d638ca91fb1e8b14dee2fa4ca22d05b
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-08-04 17:58:57 +0200

    adds ref to git diff
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- docs/make_images.sh
+++ docs/make_images.sh
@@ -6,7 +6,7 @@ retval=0
 while read -r -d $'\0' file; do
 	[ -e "$file" ] || continue
 	echo "Building image '$file'"
-	if ! git diff --exit-code HEAD^...HEAD -- "$file" > /dev/null; then
+	if ! git diff --exit-code "$1^...$1" -- "$file" > /dev/null; then
 		cp "gammasyn_doc_images_base.tex" "$buildfile"
 		filename=$(basename -- "$file")
 		filename="${filename%.*}"
