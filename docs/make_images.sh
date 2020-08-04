f277afa12a2dae414ae1d0092ba15b3637ced93c
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-08-04 17:15:01 +0200

    removes stickler errors
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- docs/make_images.sh
+++ docs/make_images.sh
@@ -3,7 +3,7 @@ set -e
 cd docs/images/tex || exit 1
 buildfile=build.tex
 retval=0
-find ./tikz_src/ -name "*.tikz" -print0 | while read -r -d $'\0' file; do
+while read -r -d $'\0' file; do
 	[ -e "$file" ] || continue
 	echo "Building image '$file'"
 	if ! git diff --exit-code HEAD^...HEAD -- "$file" > /dev/null; then
@@ -19,6 +19,6 @@ find ./tikz_src/ -name "*.tikz" -print0 | while read -r -d $'\0' file; do
 			exit $retval
 		fi
 	fi
-done
+done < <(find ./tikz_src/ -name "*.tikz" -print0)
 rm -f "$buildfile"
 exit $retval
