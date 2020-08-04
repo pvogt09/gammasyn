c6439b9aa64ef16c6264c8f19beb2b123a4431de
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-08-04 17:09:28 +0200

    corrects error handling
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- docs/make_images.sh
+++ docs/make_images.sh
@@ -2,6 +2,7 @@
 set -e
 cd docs/images/tex || exit 1
 buildfile=build.tex
+retval=0
 find ./tikz_src/ -name "*.tikz" -print0 | while read -r -d $'\0' file; do
 	[ -e "$file" ] || continue
 	echo "Building image '$file'"
@@ -11,7 +12,13 @@ find ./tikz_src/ -name "*.tikz" -print0 | while read -r -d $'\0' file; do
 		filename="${filename%.*}"
 		echo "\addtikzimage{$filename}" >> "$buildfile"
 		echo "\end{document}" >> "$buildfile"
-		pdflatex -shell-escape -interaction=nonstopmode "$buildfile"
+		pdflatex -shell-escape -interaction=nonstopmode "$buildfile" || :
+		retval=$?
+		rm -f "$buildfile"
+		if [ ! $retval ]; then
+			exit $retval
+		fi
 	fi
 done
 rm -f "$buildfile"
+exit $retval
