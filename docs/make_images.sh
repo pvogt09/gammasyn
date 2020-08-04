bb03fb827808315698f0e5482c68f1df56a5deab
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-08-04 15:38:49 +0200

    adds script for building tikz images
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

---
+++ docs/make_images.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+set -e
+cd docs/images/tex || exit 1
+buildfile=build.tex
+find ./tikz_src/ -name "*.tikz" -print0 | while read -d $'\0' file; do
+	[ -e "$file" ] || continue
+	echo "Building image '$file'"
+	if [ true ] || [ git diff --exit-code HEAD...HEAD^@ -- "$file" ]; then
+		cp "gammasyn_doc_images_base.tex" "$buildfile"
+		filename=$(basename -- "$file")
+		filename="${filename%.*}"
+		echo "\addtikzimage{$filename}" >> "$buildfile"
+		echo "\end{document}" >> "$buildfile"
+		pdflatex -shell-escape -interaction=nonstopmode "$buildfile" > /dev/null
+	fi
+done
+rm "$buildfile"
