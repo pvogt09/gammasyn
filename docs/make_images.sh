bc0d34cf1bd0b814d3ea39b94a968a7a728865b8
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-08-04 16:33:28 +0200

    removes output suppression in compilation
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- docs/make_images.sh
+++ docs/make_images.sh
@@ -5,13 +5,13 @@ buildfile=build.tex
 find ./tikz_src/ -name "*.tikz" -print0 | while read -r -d $'\0' file; do
 	[ -e "$file" ] || continue
 	echo "Building image '$file'"
-	if true || git diff --exit-code HEAD...HEAD^@ -- "$file"; then
+	if ! git diff --exit-code HEAD...HEAD^@ -- "$file"; then
 		cp "gammasyn_doc_images_base.tex" "$buildfile"
 		filename=$(basename -- "$file")
 		filename="${filename%.*}"
 		echo "\addtikzimage{$filename}" >> "$buildfile"
 		echo "\end{document}" >> "$buildfile"
-		pdflatex -shell-escape -interaction=nonstopmode "$buildfile" > /dev/null
+		pdflatex -shell-escape -interaction=nonstopmode "$buildfile"
 	fi
 done
 rm -f "$buildfile"
