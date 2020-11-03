ba24b07cc0402c3564ff3622e3e7b7e49cb3af87
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2020-11-03 13:37:50 +0100

    exchanges eval with feval
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- lib/optimization/minimize/minimize.m
+++ lib/optimization/minimize/minimize.m
@@ -1,4 +1,4 @@
-function [X, fX, i] = minimize(X, f, length, P1, P2, P3, P4, P5);
+function [X, fX, i] = minimize(X, f, length, varargin)
 
 % Minimize a continuous differentialble multivariate function. Starting point
 % is given by "X" (D by 1), and the function named in the string "f", must
@@ -36,11 +36,11 @@ EXT = 3.0;                    % extrapolate maximum 3 times the current bracket
 MAX = 20;                         % max 20 function evaluations per line search
 RATIO = 100;                                      % maximum allowed slope ratio
 
-argstr = [f, '(X'];                      % compose string used to call function
-for i = 1:(nargin - 3)
-  argstr = [argstr, ',P', int2str(i)];
-end
-argstr = [argstr, ')'];
+%argstr = [f, '(X'];                     % compose string used to call function
+%for i = 1:(nargin - 3)
+%  argstr = [argstr, ',P', int2str(i)];
+%end
+%argstr = [argstr, ')'];
 
 if max(size(length)) == 2, red=length(2); length=length(1); else red=1; end
 if length>0, S=['Linesearch']; else S=['Function evaluation']; end 
@@ -48,7 +48,7 @@ if length>0, S=['Linesearch']; else S=['Function evaluation']; end
 i = 0;                                            % zero the run length counter
 ls_failed = 0;                             % no previous line search has failed
 fX = [];
-[f1 df1] = eval(argstr);                      % get function value and gradient
+[f1, df1] = feval(f, X, varargin{:});         % get function value and gradient
 i = i + (length<0);                                            % count epochs?!
 s = -df1;                                        % search direction is steepest
 d1 = -s'*s;                                                 % this is the slope
@@ -59,7 +59,7 @@ while i < abs(length)                                      % while not finished
 
   X0 = X; f0 = f1; df0 = df1;                   % make a copy of current values
   X = X + z1*s;                                             % begin line search
-  [f2 df2] = eval(argstr);
+  [f2, df2] = feval(f, X, varargin{:});
   i = i + (length<0);                                          % count epochs?!
   d2 = df2'*s;
   f3 = f1; d3 = d1; z3 = -z1;             % initialize point 3 equal to point 1
@@ -81,7 +81,7 @@ while i < abs(length)                                      % while not finished
       z2 = max(min(z2, INT*z3),(1-INT)*z3);  % don't accept too close to limits
       z1 = z1 + z2;                                           % update the step
       X = X + z2*s;
-      [f2 df2] = eval(argstr);
+      [f2, df2] = feval(f, X, varargin{:});
       M = M - 1; i = i + (length<0);                           % count epochs?!
       d2 = df2'*s;
       z3 = z3-z2;                    % z3 is now relative to the location of z2
@@ -113,7 +113,7 @@ while i < abs(length)                                      % while not finished
     end
     f3 = f2; d3 = d2; z3 = -z2;                  % set point 3 equal to point 2
     z1 = z1 + z2; X = X + z2*s;                      % update current estimates
-    [f2 df2] = eval(argstr);
+    [f2, df2] = feval(f, X, varargin{:});
     M = M - 1; i = i + (length<0);                             % count epochs?!
     d2 = df2'*s;
   end                                                      % end of line search
