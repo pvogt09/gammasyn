660bbf36097dcd1da9e622b9d4aeebebd8ab8bb0
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2021-01-22 09:50:46 +0100

    included feedthrough to merit function
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- +control/+design/+gamma/private/objective_decoupling.m
+++ +control/+design/+gamma/private/objective_decoupling.m
@@ -21,9 +21,11 @@ function [J, gradJ, hessJ] = objective_decoupling(R, ~, F, systems, dimensions, 
 	number_references = dimensions.references;
 	V_invariant = dimensions.V_invariant;
 	m_invariant = dimensions.m_invariant;
+	hasfeedthrough = dimensions.hasfeedthrough_decoupling;
+	number_decouplingconditions = dimensions.number_decouplingconditions;
 	tf_structure = options.decouplingcontrol.tf_structure;
 	r = reshape(R, [], 1);
-	
+
 	J = 0;
 	gradJ = zeros(number_controls*(number_measurements + number_measurements_xdot + number_references), 1);
 	hessJ = zeros(number_controls*(number_measurements + number_measurements_xdot + number_references), number_controls*(number_measurements + number_measurements_xdot + number_references));
@@ -31,36 +33,59 @@ function [J, gradJ, hessJ] = objective_decoupling(R, ~, F, systems, dimensions, 
 		A  = systems(ii).A;
 		B  = systems(ii).B;
 		C  = systems(ii).C;
+		C_ref = systems(ii).C_ref;
 		D_ref = systems(ii).D_ref;
 		for jj = 1:number_references
 			g_structure = tf_structure(:, jj); %#ok<PFBNS>
+			Cjj = C_ref(g_structure == 0, :);
 			Djj = D_ref(g_structure == 0, :);
-			if any(Djj(:) ~= 0)
-				error('control:design:gamma:decoupling', 'Merit function for decoupling not implemented for feedthrough.');
-			end
 			m = m_invariant(jj); %#ok<PFBNS>
 			if m == 0
 				continue;
 			end
 			Q = V_invariant(:, 1:m, ii, jj); %#ok<PFBNS>
 			Q_orth = V_invariant(:, m + 1:end, ii, jj);
-			
-			X = kron(Q'*C', Q_orth'*B);
-			z = reshape(Q_orth'*A*Q, (number_states - m)*m, 1);
-			e = X*r - z;
+
+			if hasfeedthrough(jj) %#ok<PFBNS>
+				X_R = [
+					kron(Q'*C', Q_orth'*B);
+					kron(Q'*C', Djj)
+				];
+				z_R = [
+					reshape(Q_orth'*A*Q, (number_states - m)*m, 1);
+					reshape(Cjj*Q, number_decouplingconditions(jj)*m, 1)
+				]; %#ok<PFBNS>
+			else
+				X_R = kron(Q'*C', Q_orth'*B);
+				z_R = reshape(Q_orth'*A*Q, (number_states - m)*m, 1);
+			end
+			e_R = X_R*r - z_R;
+			J_R = e_R.'*e_R;
+
 			f = F(:, jj); %#ok<PFBNS>
-			J = J + e.'*e + sum((Q_orth.'*B*f).^2);
+			if hasfeedthrough(jj)
+				X_F = [
+					Q_orth.'*B;
+					Djj
+				];
+			else
+				X_F = Q_orth.'*B;
+			end
+			e_F = X_F*f;
+			J_F = e_F.'*e_F;
+			
+			J = J + J_R + J_F;
 			if needsgradient
 				gradJ = gradJ + 2*[
-					r.'*(X.'*X) - z.'*X, zeros(1, number_controls*number_measurements_xdot), zeros(1, (jj - 1)*number_controls), f.'*B.'*(Q_orth*Q_orth.')*B, zeros(1, (number_references - jj)*number_controls)
+					r.'*(X_R.'*X_R) - z_R.'*X_R, zeros(1, number_controls*number_measurements_xdot), zeros(1, (jj - 1)*number_controls), f.'*(X_F.'*X_F), zeros(1, (number_references - jj)*number_controls)
 				].';
 			end
 			if needshessian
 				hessJ_tmp = 2*blkdiag(...
-					X.'*X,...
+					X_R.'*X_R,...
 					zeros(number_controls*number_measurements_xdot, number_controls*number_measurements_xdot),...
 					zeros((jj - 1)*number_controls, (jj - 1)*number_controls),...
-					B.'*(Q_orth*Q_orth.')*B,...
+					X_F.'*X_F,...
 					zeros((number_references - jj)*number_controls, (number_references - jj)*number_controls)...
 				);
 				hessJ = hessJ + hessJ_tmp;
